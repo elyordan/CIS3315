e0bc0dd7c2d795bfc8e46cb0337f62fcd111aec9
elyordan <elyordan_21@hotmail.com>
2021-02-25 03:09:51 -0500

    finish methods getView and getdropdwnview

--- Lab3/app/src/main/java/edu/temple/ImageActivity/CarAdapter.java
+++ Lab3/app/src/main/java/edu/temple/ImageActivity/CarAdapter.java
@@ -1,9 +1,13 @@
 package edu.temple.ImageActivity;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -36,6 +40,43 @@ public class CarAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        TextView textView = new TextView(context);
+        textView.setText(items.get(position));
+
+        textView.setPadding(10,10,10,10);
+        textView.setTextSize(25);
+        textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
+        return textView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
+        ImageView imageView;
+
+        LinearLayout linearLayout;
+
+        if(convertView == null){
+            linearLayout = new LinearLayout(context);
+            textView = new TextView(context);
+            imageView = new ImageView(context);
+            textView.setTextSize(20);
+            textView.setPadding(10,10,10,10);
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.addView(imageView);
+            linearLayout.addView(textView);
+            imageView.getLayoutParams().height = 150;
+            imageView.getLayoutParams().width = 150;
+        } else {
+            linearLayout = (LinearLayout) convertView;
+            imageView = (ImageView) linearLayout.getChildAt(0);
+            textView = (TextView) linearLayout.getChildAt(1);
+        }
+
+        imageView.setImageResource(carImage[position]);
+        textView.setText(items.get(position));
+
+        return linearLayout;
     }
 }
