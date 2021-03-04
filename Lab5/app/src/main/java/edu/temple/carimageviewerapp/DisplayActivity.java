312543bb0682df436cc3e72dd60ba11a7df410aa
elyordan <elyordan_21@hotmail.com>
2021-03-04 12:17:24 -0500

    copied lab4 to start lab5 on new branch

---
+++ Lab5/app/src/main/java/edu/temple/carimageviewerapp/DisplayActivity.java
@@ -0,0 +1,34 @@
+package edu.temple.carimageviewerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    TextView textView;
+    ImageView imageview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+        imageview = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        int[] carResIds = new int[]{R.drawable.white, R.drawable.chiron, R.drawable.huracan, R.drawable.laferrari,
+                R.drawable.gtr, R.drawable.gt3, R.drawable.amg};
+
+        Intent intent = getIntent();
+
+        String carName = intent.getStringExtra(SelectionActivity.EXTRA_NAME);
+        int position = intent.getIntExtra(SelectionActivity.EXTRA_NUMBER, 0);
+
+        textView.setText(carName);
+        imageview.setImageResource(carResIds[position]);
+    }
+}
