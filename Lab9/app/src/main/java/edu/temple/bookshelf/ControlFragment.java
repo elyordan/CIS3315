abe8ef242f975857604ff97dc1bf0156227f0181
yuniorlopez <elyordan_21@hotmail.com>
2021-04-15 20:40:45 -0400

    Most of the functionality is done but the book does not keep playing on activity restart

--- Lab9/app/src/main/java/edu/temple/bookshelf/ControlFragment.java
+++ Lab9/app/src/main/java/edu/temple/bookshelf/ControlFragment.java
@@ -1,5 +1,6 @@
 package edu.temple.bookshelf;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,55 +11,48 @@ import android.widget.TextView;
 
 public class ControlFragment extends Fragment {
     private boolean playing;
-    private Book currBook;
-    private MainActivity parentAct;
-    private Button playButton, pauseButton, stopButton;
-    private TextView nowPlaying;
+    private Book currentBook;
+    private MainActivity parentActivity;
+    private TextView currentlyPlaying;
 
     public ControlFragment() {
         //Required empty default constructor
     }
 
     public static ControlFragment newInstance() {
-        ControlFragment myfrag = new ControlFragment();
-        myfrag.playing = false;
-        return myfrag;
+        ControlFragment controlFragment = new ControlFragment();
+        controlFragment.playing = false;
+        return controlFragment;
     }
 
     public static ControlFragment newInstance(MainActivity parent) {
-        ControlFragment myfrag = new ControlFragment();
-        myfrag.playing = false;
-        myfrag.parentAct = parent;
-        return myfrag;
+        ControlFragment controlFragment = new ControlFragment();
+        controlFragment.playing = false;
+        controlFragment.parentActivity = parent;
+        return controlFragment;
     }
 
     public void setPlayBook(Book book) {
-        currBook = book;
+        currentBook = book;
 
-        if(currBook != null)
-            playing = true;
-        else
-            playing = false;
-
-        updateHeader();
+        playing = currentBook != null;
+        updateNowPlayingBook();
     }
 
-    private void updateHeader()
-    {
+    private void updateNowPlayingBook() {
         String header = getString(R.string.nowPlaying);
 
-        if(currBook != null)
-            header += currBook.getTitle();
+        if(currentBook != null)
+            header += currentBook.getTitle();
 
-        if(nowPlaying != null)
-            nowPlaying.setText(header);
+        if(currentlyPlaying != null)
+            currentlyPlaying.setText(header);
     }
 
-    private void playPause()
-    {
+    private void playAndPause() {
         playing = !playing;
 
-        parentAct.pause(); //Pause handles the logic for if already paused or not
+        parentActivity.pause(); //Pause handles the logic for if already paused or not
     }
 
 
@@ -66,43 +60,19 @@ public class ControlFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_control, null);
+        @SuppressLint("InflateParams") View v = inflater.inflate(R.layout.fragment_control, null);
 
-        nowPlaying = v.findViewById(R.id.nowPlaying);
-        updateHeader();
+        currentlyPlaying = v.findViewById(R.id.nowPlaying);
+        updateNowPlayingBook();
 
-        //Set up Listener for play/pause button
-        playButton = v.findViewById(R.id.playButtom);
-        playButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                playPause();
-            }
-        });
+        Button playButton = v.findViewById(R.id.playButtom);
+        playButton.setOnClickListener(v1 -> parentActivity.playThisBook());
 
-        stopButton = v.findViewById(R.id.stopButtom);
-        stopButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                parentAct.stop();
-            }
-        });
+        Button pauseButton = v.findViewById(R.id.pauseButtom);
+        pauseButton.setOnClickListener(v12 -> playAndPause());
 
-//        //Set up Listener for Search
-//        search = v.findViewById(R.id.search);
-//        search.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                parentAct.StartBookSearch();
-//            }
-//        });
-//
+        Button stopButton = v.findViewById(R.id.stopButtom);
+        stopButton.setOnClickListener(v13 -> parentActivity.stop());
         return v;
     }
 }
--- Lab9/app/src/main/java/edu/temple/bookshelf/MainActivity.java
+++ Lab9/app/src/main/java/edu/temple/bookshelf/MainActivity.java
@@ -48,14 +48,14 @@ public class MainActivity extends AppCompatActivity {
     String webURL;
     RequestQueue requestQueue;
     SharedPreferences sharedPref;
-    private ControlFragment controls;
-    private AudiobookService.MediaControlBinder abService;
+    private ControlFragment controlFragment;
+    private AudiobookService.MediaControlBinder audioBookService;
     private SeekBar seekbar;
     private int progress;
     private boolean playing;
     private TimerTask timertask;
     private Timer timer;
-    public ServiceConnection myConnection;
+    public ServiceConnection serviceConnection;
 
 
     @Override
@@ -68,29 +68,29 @@ public class MainActivity extends AppCompatActivity {
         Resources res = getResources();
         webURL = res.getString(R.string.bookURL);
 
-        myConnection = new ServiceConnection()
+        serviceConnection = new ServiceConnection()
         {
             public void onServiceConnected(ComponentName className, IBinder binder)
             {
-                abService = (AudiobookService.MediaControlBinder)binder;
+                audioBookService = (AudiobookService.MediaControlBinder)binder;
             }
 
             public void onServiceDisconnected(ComponentName className) {
-                abService = null;
+                audioBookService = null;
             }
         };
 
-        Intent intent = null;
+        Intent intent;
         intent = new Intent(this, AudiobookService.class);
-        bindService(intent, myConnection, Context.BIND_AUTO_CREATE);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
 
         requestQueue = Volley.newRequestQueue(this);
 
         booksCollection = new BookList(this, bookArrayList);
 
         currentDetails = BookDetailsFragment.newInstance(currentBook);
-        controls = ControlFragment.newInstance(this);
-        Fill_The_Fragment(R.id.fragment_control, controls, false);
+        controlFragment = ControlFragment.newInstance(this);
+        Fill_The_Fragment(R.id.fragment_control, controlFragment, false);
         Fill_The_Fragment(R.id.book_list_fragment, BookListFragment.newInstance(booksCollection, this), false);
 
 
@@ -111,8 +111,6 @@ public class MainActivity extends AppCompatActivity {
         //Shared prefs stored the necessary data to resume our audiobook on restart
         String bookTitle = sharedPref.getString("bookTitle", null);
 
-        //If bookTitle is null, we'll assume we weren't playing anything
-        //Otherwise, we'll create a new book object to represent what we were playing
         if(bookTitle != null)
         {
             int bookID = sharedPref.getInt("bookID", 0);
@@ -122,16 +120,15 @@ public class MainActivity extends AppCompatActivity {
             String bookURL = sharedPref.getString("bookURL", "");
             currentBook = new Book( bookTitle, bookAuthor, bookURL, bookID, bookDuration);
             playBook = currentBook;
-            controls.setPlayBook(playBook); //Let our Control Fragment Know
+            controlFragment.setPlayBook(playBook);
 
-            //Set up a delay to start playing so we can dodge that null reference
             TimerTask task = new TimerTask()
             {
                 public void run()
                 {
                     playing = true;
                     setupSeekBar();
-                    abService.play(playBook.getId(), progress);
+                    audioBookService.play(playBook.getId(), progress);
                 }
             };
 
@@ -140,15 +137,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //----------------------------------------------------------------------------------------------
-    // Helper Methods for Fragment and Activity Management
-    //----------------------------------------------------------------------------------------------
-
-    //In Java we can't reuse a timer task for some unfathomable reason, so we have to redefine
-    //it every time its called
-    private void createTimerTask()
-    {
-        //ReDefine our TimerTask
+    private void createTimerTask() {
         timertask = new TimerTask()
         {
             public void run()
@@ -159,15 +148,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onClickButtonMethod() {
-
         searchButton = findViewById(R.id.search);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Start_Book_Search();
-            }
-        });
+        searchButton.setOnClickListener(v -> Start_Book_Search());
     }
 
     public void Book_Selection(int position) {
@@ -205,10 +187,9 @@ public class MainActivity extends AppCompatActivity {
         super.onConfigurationChanged(newConfig);
         setContentView(R.layout.activity_main);
 
-        //Reinitialize the Control Fragment
-        controls = ControlFragment.newInstance(this);
-        Fill_The_Fragment(R.id.fragment_control, controls, false);
-        controls.setPlayBook(playBook);
+        controlFragment = ControlFragment.newInstance(this);
+        Fill_The_Fragment(R.id.fragment_control, controlFragment, false);
+        controlFragment.setPlayBook(playBook);
         setupSeekBar();
 
         twoWindows = (findViewById(R.id.book_details_fragment) != null);
@@ -233,37 +214,28 @@ public class MainActivity extends AppCompatActivity {
     private void UpdateListOfBooks(String searchTerm) {
         String urlString = webURL + searchTerm;
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, urlString, null, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                try {
-                    ArrayList<Book> newList = new ArrayList<Book>();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, urlString, null, (Response.Listener<JSONArray>) response -> {
+            try {
+                ArrayList<Book> newList = new ArrayList<>();
 
-                    for (int i = 0; i < response.length(); i++) {
-                        JSONObject currObj = (JSONObject) response.getJSONObject(i);
-                        int id = currObj.getInt("id");
-                        int duration = currObj.getInt("duration");
-                        String title = currObj.getString("title");
-                        String author = currObj.getString("author");
-                        String coverURL = currObj.getString("cover_url");
-                        newList.add(new Book(title, author, coverURL, id, duration));
-                    }
-
-                    booksCollection = new BookList(MainActivity.this, newList);
-                    Fragment list = BookListFragment.newInstance(booksCollection, MainActivity.this);
-                    Fill_The_Fragment(R.id.book_list_fragment, list, false);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject currObj = (JSONObject) response.getJSONObject(i);
+                    int id = currObj.getInt("id");
+                    int duration = currObj.getInt("duration");
+                    String title = currObj.getString("title");
+                    String author = currObj.getString("author");
+                    String coverURL = currObj.getString("cover_url");
+                    newList.add(new Book(title, author, coverURL, id, duration));
                 }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
 
+                booksCollection = new BookList(MainActivity.this, newList);
+                Fragment list = BookListFragment.newInstance(booksCollection, MainActivity.this);
+                Fill_The_Fragment(R.id.book_list_fragment, list, false);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, (Response.ErrorListener) Throwable::printStackTrace);
         requestQueue.add(jsonArrayRequest);
 
     }
@@ -285,14 +257,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //----------------------------------------------------------------------------------------------
-    // Helper Methods for AudioBook Playing / Pausing / Stopping
-    //----------------------------------------------------------------------------------------------
 
-    //Pauses pauses the current playback, or plays it if already paused.
-    public void pause()
-    {
-        abService.pause();
+    public void pause() {
+        audioBookService.pause();
 
         if(playBook != null)
             playing = !playing;
@@ -304,25 +271,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //playThisBook() is called BookDetailsFragment. It changes which book is currently loaded
-    //and playing by the audiobookservice
-    public void playThisBook()
-    {
-        //Button is visible before book is selected, deal with null reference
+    public void playThisBook() {
+
         if(currentBook == null)
             return;
 
-        if(abService == null)
+        if(audioBookService == null)
             return;
 
-        abService.stop(); //Stop a currently playing audiobook
-        progress = 0; //Reset our Progress Tracking
-        playBook = currentBook; //Track What Book is Playing
-        controls.setPlayBook(playBook); //Let our Control Fragment Know
-        setupSeekBar(); //Reset our Seekbar
-        playing = true; //Track that we're playing a book
+        audioBookService.stop();
+        progress = 0;
+        playBook = currentBook;
+        controlFragment.setPlayBook(playBook);
+        setupSeekBar();
+        playing = true;
 
-        //Write the ID of our Current Book
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("bookID", playBook.getId());
         editor.putString("bookTitle", playBook.getTitle());
@@ -332,74 +295,63 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt("progress", 0);
         editor.apply();
 
-        //Schedule a timer for our seekbar
         createTimerTask();
         long delay = 2000;
         timer.schedule(timertask, delay);
 
-        abService.play(playBook.getId()); //Actually Call the Service
+        audioBookService.play(playBook.getId());
     }
 
-    //seekTimeUpdate() handles passive updates to the seekbar progress when playing
-    public void seekTimeUpdate()
-    {
-        //Update Our Local Progress
+
+    public void seekTimeUpdate() {
+
         progress += 2;
         seekbar.setProgress(progress);
 
-        //Save our Local Progress
+
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("progress", progress);
         editor.apply();
 
-        //Schedule another timer
+
         createTimerTask();
         long delay = 2000;
         timer.schedule(timertask, delay);
     }
 
-    //SetupSeekBar sets up the listener for the seek bar, its called in its own method due to the
-    //frequency that the ControlFragment is reinitialized
-    public void setupSeekBar()
-    {
+    public void setupSeekBar() {
         seekbar = findViewById(R.id.seekBar);
         seekbar.setMax(playBook.getDuration());
         seekbar.setProgress(progress);
 
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int newProgress, boolean fromUser)
-            {
-                if (fromUser) //We check this because the seekbar will update periodically
-                {
-                    abService.seekTo(newProgress);
+            public void onProgressChanged(SeekBar seekBar, int newProgress, boolean fromUser) {
+                if (fromUser) {
+                    audioBookService.seekTo(newProgress);
                     progress = newProgress;
                 }
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar)
-            {
+            public void onStartTrackingTouch(SeekBar seekBar) {
                 //onProgressChanged will handle this. This can do nothing.
             }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar)
-            {
+            public void onStopTrackingTouch(SeekBar seekBar) {
                 //onProgressChanged will handle this. This can do nothing.
             }
         });
     }
 
-    //Stop() stops the currently playing book and sets playBook to null
-    public void stop()
-    {
-        abService.stop();
+
+    public void stop() {
+        audioBookService.stop();
         playing = false;
 
         playBook = null;
-        controls.setPlayBook(null);
+        controlFragment.setPlayBook(null);
 
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("bookTitle", null);
